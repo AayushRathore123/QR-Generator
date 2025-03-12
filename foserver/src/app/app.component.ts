c8fa87f5005ba849242596f4188ba744c56a8b49
Shreya Sharma <shreyasharma1201@gmail.com>
2025-03-12 21:06:05 +0530

    added login, register, and navbar components with initial implementation

--- foserver/src/app/app.component.ts
+++ foserver/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./layout/auth/login/login.component";
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { RegisterComponent } from "./layout/auth/register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [LoginComponent, NavbarComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
