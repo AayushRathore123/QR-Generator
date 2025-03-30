4750066ef4db0e953ba29287de2e0768197b4a85
Shreya Sharma <shreyasharma1201@gmail.com>
2025-03-30 20:47:11 +0530

    modified user authentication

--- foserver/src/app/layout/auth/login/login.component.ts
+++ foserver/src/app/layout/auth/login/login.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { DisableCutCopyPasteDirective } from '../../../shared/directives/disablecopypaste/disable-cut-copy-paste.directive';
 import { EncryptDecryptService } from '../../../shared/services/encrypt-decrypt/encrypt-decrypt.service';
@@ -9,75 +15,71 @@ import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,CommonModule,ReactiveFormsModule,DisableCutCopyPasteDirective],
+  imports: [
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    DisableCutCopyPasteDirective,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  loginForm?: any;
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  showPassword = false;
   submitted = false;
-  showpassword = false;
 
-  // constructor(private fb: FormBuilder, private authService: AuthService, private router: Router, private _toastr: ToastrService) { }
-  constructor(private fb: FormBuilder,private router: Router, private encryptdecryptservice: EncryptDecryptService, private _authService: AuthService, private toastr : ToastrService){}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private encryptDecryptService: EncryptDecryptService,
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) {}
+
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      username:['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      password: ['']
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
     });
   }
 
-  seepassword(flag:boolean) {
-    if( this.loginForm.controls['password'].value!=""){
-      this.showpassword = true;
-      if (flag == true)
-        this.showpassword = false;
-      else
-        setTimeout(() => {
-          this.showpassword = false;
-        }, 3000);
-    }
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
   }
 
   onSubmit(): void {
-    console.log('loginform', this.loginForm)
-    if (this.loginForm.valid) {
-      // let jsonData = {
-      //   'user_name': this.loginForm.controls['username'].value.trim().toLowerCase(),
-      //   'password': this.encryptdecryptservice.encrypt(this.loginForm.controls['password'].value)
-      // }
-
-      let jsonData = {
-        "user_name": "test@gmail.com",
-          "password":"test"
-      }
-
-      this._authService.login(jsonData).subscribe(resp=>{
-        console.log('resp',resp)
-        if(resp && resp.errCode===0){
-          this.toastr.success(resp.msg,'Successs');
-          this._authService.setUser(resp.data_rec);
-          this.router.navigate(['/layout/home']);
-        }
-        else{
-          this.toastr.error(resp.msg,'Error');
-        }
-      })
-      // this.authService.login(username, password).subscribe(resp => {
-      //   if (resp.status == 'Success') {
-      //     this._toastr.success(resp.message, 'SUCCESS')
-
-      //     // alert(resp.message);
-      //     console.log("Login successful");
-      //     this.router.navigate(['/products']);
-      //   } else {
-      //     this._toastr.error(resp.message, 'ERROR')
-      //     // alert(resp.message);
-      //     console.log("Login failed");
-      //   }
-      // });
-    }
     this.submitted = true;
 
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    // const jsonData = {
+    //   user_name: this.loginForm.value.username.trim().toLowerCase(),
+    //   password: this.encryptDecryptService.encrypt(this.loginForm.value.password),
+    // };
+
+    let jsonData = {
+      user_name: 'test@gmail.com',
+      password: 'test',
+    };
+
+    this.authService.login(jsonData).subscribe(
+      (resp) => {
+        if (resp && resp.errCode === 0) {
+          this.toastr.success(resp.msg, 'Success');
+          this.authService.setUser(resp.data_rec);
+          this.router.navigate(['/layout/home']);
+        } else {
+          this.toastr.error(resp.msg, 'Error');
+        }
+      },
+      (error) => {
+        this.toastr.error('Something went wrong. Please try again.', 'Error');
+      }
+    );
+
+    console.log("hi",this.authService.getStoredUser() );
   }
 }
