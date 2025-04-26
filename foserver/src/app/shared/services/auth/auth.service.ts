79926a30e077c3ea357fc54d4b7e7bae6de233ef
Shreya Sharma <shreyasharma1201@gmail.com>
2025-04-26 18:46:13 +0530

    Removed unused API calls, added a new username key in localStorage, and introduced new API calls for getting and updating user data.

--- foserver/src/app/shared/services/auth/auth.service.ts
+++ foserver/src/app/shared/services/auth/auth.service.ts
@@ -7,6 +7,13 @@ import { environment } from "../../../../environment/environment";
 })
 export class AuthService {
   apiUrl = environment.apiUrl;
+  private loginURL =environment.apiUrl+'login';
+  private registerURL =environment.apiUrl+'register';
+  private getDataURL =environment.apiUrl+'user_details/get';
+  private updateDataURL = environment.apiUrl+'user_details/update';
+  private changeUserPasswordURL = environment.apiUrl+'change';
+  private deleteUserURL = environment.apiUrl+'delete';
+
   private userSubject = new BehaviorSubject<any>(null); 
   user = this.userSubject.asObservable();
   private loggedInSubject = new BehaviorSubject<boolean>(this.isAuthenticatedUser());
@@ -16,21 +23,23 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   register(jsonData: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl+'register', jsonData).pipe(map((data) => {
+    return this.http.post<any>(this.registerURL, jsonData).pipe(map((data) => {
       return data;
     }));
   }
 
   login(jsonData:any): Observable<any>{
-    return this.http.post<any>(this.apiUrl+'login',jsonData).pipe(map((data) => {
+    return this.http.post<any>(this.loginURL,jsonData).pipe(map((data) => {
       if(data.errCode==0){
         localStorage.setItem('Token', data.access_token);
         const payloadBase64 = data.access_token.split('.')[1];
         const decodedPayload = JSON.parse(atob(payloadBase64));
         const subData = JSON.parse(decodedPayload.sub);
-        const userId = subData.user_id;
+        const userId = data.user_id;
+        const userName = data.user_name;
         const exp = decodedPayload.exp;
-        localStorage.setItem('UserId',userId);
+        localStorage.setItem('userId',userId);
+        localStorage.setItem('username',userName);
         localStorage.setItem('expiry', exp);
         this.userSubject.next(data.data_rec); 
         this.loggedInSubject.next(true);
@@ -52,16 +61,44 @@ export class AuthService {
   logout(){
     this.loggedInSubject.next(false);
     localStorage.removeItem("Token");
-    localStorage.removeItem("UserId");
+    localStorage.removeItem("userId");
     localStorage.removeItem("expiry");
+    localStorage.removeItem("username");
     this.userSubject.next(null);
   }
 
   getUserId(){
-    return localStorage.getItem("UserId");
+    return localStorage.getItem("userId");
   }
 
   getToken(){
     return localStorage.getItem("Token");
+  }
+
+  getUserName(){
+    return localStorage.getItem("username");
+  }
+
+  getUserData(user_id:any){
+    const url = `${this.getDataURL}?user_id=${user_id}`;  
+    return this.http.get(url);  
+  }
+
+  updatedUserData(jsonData: any): Observable<any>{
+    return this.http.post<any>(this.updateDataURL, jsonData).pipe(map((data) => {
+      return data;
+    }));
+  }
+
+  changeUserPassword(jsonData: any): Observable<any>{
+    return this.http.post<any>(this.changeUserPasswordURL, jsonData).pipe(map((data) => {
+      return data;
+    }));
+  }
+
+  deleteUser(user_id:any){
+    const url = `${this.deleteUserURL}?user_id=${user_id}`;  
+    this.logout();
+    return this.http.get(url); 
   }
 }
