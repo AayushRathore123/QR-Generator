04903b14c562e5af9f7b9e2be73d5b2a3caee30d
Shreya Sharma <shreyasharma1201@gmail.com>
2025-06-29 22:32:23 +0530

    [fix]: fixed logic of generate-env

--- foserver/generate-env.js
+++ foserver/generate-env.js
@@ -1,7 +1,12 @@
-// generate-env.js
 const fs = require("fs");
+const path = require("path");
 
-const targetPath = "./src/environment/environment.ts";
+const targetDir = path.join(__dirname, "src/environment");
+const targetPath = path.join(targetDir, "environment.ts");
+
+if (!fs.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir, { recursive: true });
+}
 
 const envConfigFile = `
 export const environment = {
